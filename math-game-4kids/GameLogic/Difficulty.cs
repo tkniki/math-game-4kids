2287fd9aba0659879c9439250fa6586ff1cef474
tkniki <trankimniki@gmail.com>
2022-11-16 22:54:31 +0100

    Difficulty enum

---
+++ math-game-4kids/GameLogic/Difficulty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_game_4kids.GameLogic
+{
+    public enum Difficulty
+    {
+        EASY, MEDIUM, HARD
+    }
+}
